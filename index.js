15a4f1b4e0ea1ef9c5712f2946db2f9f58026d35
Kyle Kelley <rgbkrk@gmail.com>
2015-12-08 19:25:03 -0600

    Apparently path isn't global in some environments.
    /me whistles.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var path = require('path');
 
 var demoBin  = fs.readFileSync(path.join(__dirname, "DemoMode.bin"));
 var worldMap = fs.readFileSync(path.join(__dirname, "CompanionWorldMap.png"));
